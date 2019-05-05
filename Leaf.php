0bf25ffb1b70be77b5bbaa2791b7e610c4fb097d
Serafim <nesk@xakep.ru>
2019-05-05 21:10:48 +0300

    Add multirepo build script

---
+++ Leaf.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of Phplrt package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Phplrt\Ast;
+
+/**
+ * Class Leaf
+ */
+class Leaf extends Node implements LeafInterface
+{
+    /**
+     * @var string[]
+     */
+    private $value;
+
+    /**
+     * Leaf constructor.
+     *
+     * @param string $name
+     * @param string|string[] $value
+     * @param int $offset
+     */
+    public function __construct(string $name, $value, int $offset = 0)
+    {
+        parent::__construct($name, $offset);
+
+        $this->value = (array)$value;
+    }
+
+    /**
+     * @param int $group
+     * @return string|null
+     */
+    public function getValue(int $group = 0): ?string
+    {
+        return $this->value[$group] ?? null;
+    }
+}
---
+++ Node.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * This file is part of Phplrt package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Phplrt\Ast;
+
+use Phplrt\Ast\Dumper\HoaDumper;
+use Phplrt\Ast\Dumper\XmlDumper;
+
+/**
+ * Class Node
+ */
+abstract class Node implements NodeInterface
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var int
+     */
+    protected $offset;
+
+    /**
+     * Node constructor.
+     *
+     * @param string $name
+     * @param int $offset
+     */
+    public function __construct(string $name, int $offset = 0)
+    {
+        $this->name = $name;
+        $this->offset = $offset;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOffset(): int
+    {
+        return $this->offset;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        $dumper = \class_exists(\DOMDocument::class)
+            ? new XmlDumper()
+            : new HoaDumper();
+
+        return $dumper->dump($this);
+    }
+}
